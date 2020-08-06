aeed24887998ee645a502b66885c461c72650834
yuliya_sharapa <juliya.sharapa@gmail.com>
2020-08-05 22:12:26 -0300

    se agrera parte de Last Product en DB

--- src/components/content/Product/LastProduct.js
+++ src/components/content/Product/LastProduct.js
@@ -1,47 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-
-export default class LastProduct extends React.Component {
+export default class LastProduct extends Component {
 
     constructor(props){
         super(props);
-        this.state = {}
-    }
-
-    componentDidMount() {
-        this.callAPI('http://localhost:3030/api/products')
+        this.state = {
+            product:{}
         }
-
-    callAPI = (urlApi) => {
-        fetch(urlApi)
-        .then(res => res.json())
-        .then((result) => {
-            console.log(result);
-            this.findLastProduct(result)
-        })
-        .catch(error => console.log(error))
     }
 
-    findLastProduct = (data) => {
+    componentDidMount(){
+
+        fetch('http://localhost:3030/api/products')
+            .then(res=>res.json())
+            .then(result=>{
+                this.setState({
+                    product: result.products.slice(-1)[0]
+                });
+            });
 
     }
 
     render(){
-    return (
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '25rem'}} src="assets/images/product_dummy.svg" alt="dummy"/>
+        var {product} = this.state;
+        return (
+            <div className="col-lg-6 mb-4">
+                <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                        <h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
-                    <a target="_blank" rel="nofollow" href="/">View product detail</a>
+                    <div className="card-body">
+                        <div className="text-center">
+                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '25rem'}} src={`http://localhost:3030/${product.urlImagen}`} alt={product.name}/>
+                        </div>
+                        <p><strong>{product.name}.</strong> {product.descripcion}</p>
+                        <a target="_blank" rel="nofollow" href="/">View product detail</a>
+                    </div>
                 </div>
             </div>
-        </div>
     )
     }
 }
